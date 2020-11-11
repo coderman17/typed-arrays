4dac3aa37df261cefc4ddf2ff3340ae80fff565f
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-11-11 20:11:46 +0000

    finalised stringToString

--- src/StringToStringArray.php
+++ src/StringToStringArray.php
@@ -4,10 +4,16 @@ declare(strict_types = 1);
 
 namespace TypedArrays;
 
+use TypedArrays\Traits\StringKeyAnyValue;
+
 class StringToStringArray extends TypedKeyValueArray
 {
+    use StringKeyAnyValue;
+
     public function setItem(string $key, string $value): void
     {
+        $this->validateKey($key);
+
         $this->items[$key] = $value;
     }
 }
