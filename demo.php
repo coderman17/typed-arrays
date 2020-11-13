1ebc1ca2acbf92a54b969fc5f4c8323c1e62d84d
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-11-13 21:41:35 +0000

    demo tweaks

--- demo.php
+++ demo.php
@@ -15,7 +15,7 @@ $intToBlogPostArray->setItem(1, new BlogPost('This is a blog post about food'));
 
 
 function show_blog_posts_content(IntToBlogPostArray $intToBlogPostArray): void      //Functions can unequivocally declare the type of array object required
-{
+{                                                                                   //The array object can be used with assurance that it will only contain int keys and BlogPost objects
     echo "\nThe contents of the BlogPosts in the typed array object are:\n";
 
     foreach ($intToBlogPostArray as $blogPost) {                                    //The IntToBlogPostArray object can be used like an array in a foreach loop
@@ -25,10 +25,10 @@ function show_blog_posts_content(IntToBlogPostArray $intToBlogPostArray): void  
 
     echo "\nThe getItems() method on the typed array object gives:\n";
 
-    var_dump($intToBlogPostArray->getItems());                                      //Or, the array can be retrieved as a whole via getItems()
+    var_dump($intToBlogPostArray->getItems());                                      //Or, the array that the object holds can be retrieved as a whole via getItems()
 
-//    $intToBlogPostArray->pushItem(new \stdClass());                                 //Adding objects of incorrect class throw a TypeError
+//    $intToBlogPostArray->pushItem(new \stdClass());                                 //Adding objects of incorrect class throws a TypeError
 }
 
 
-show_blog_posts_content($intToBlogPostArray);                                       //Pass array to function with full type hinting
+show_blog_posts_content($intToBlogPostArray);                                       //Passing variables to the function gives full type hinting of the array object required
