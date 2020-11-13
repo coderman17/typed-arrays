02a1748b571947175d0ff920084e0c5b47fa7dbb
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-11-13 20:24:10 +0000

    demo tweaks

--- demo.php
+++ demo.php
@@ -2,26 +2,33 @@
 
 declare(strict_types = 1);
 
+namespace TypedArrays\Demo;
+
 require 'vendor/autoload.php';
 
-use TypedArrays\Demo\BlogPost;                                                      //The class of object we want to put inside a Typed Array
-use TypedArrays\Demo\IntToBlogPostArray;                                            //A simple extension of the IntToClassArray abstract class
 
-function show_blog_posts_content (IntToBlogPostArray $intToBlogPostArray): void     //Function declares type of array object required
+$intToBlogPostArray = new IntToBlogPostArray();                                     //Make new Typed Array object
+
+
+$intToBlogPostArray->pushItem(new BlogPost('This is a blog post about cats'));      //Can add objects of correct class via pushItem()
+$intToBlogPostArray->setItem(1, new BlogPost('This is a blog post about food'));    //Or via setItem(), to specify the key too
+
+
+function show_blog_posts_content(IntToBlogPostArray $intToBlogPostArray): void      //Functions can unequivocally declare the type of array object required
 {
-    foreach ($intToBlogPostArray as $blogPost){                                     //The IntToBlogPostArray object can be used like an array in a foreach loop
-        echo $blogPost->getContent() . "\n";
+    echo "\nThe contents of the BlogPosts in the typed array object are:\n";
+
+    foreach ($intToBlogPostArray as $blogPost) {                                    //The IntToBlogPostArray object can be used like an array in a foreach loop
+        echo ' - ' . $blogPost->getContent() . "\n";
     }
 
-    $blogPosts = $intToBlogPostArray->getItems();                                   //Or, the array can be retrieved as a whole via getItems()
+
+    echo "\nThe getItems() method on the typed array object gives:\n";
+
+    var_dump($intToBlogPostArray->getItems());                                      //Or, the array can be retrieved as a whole via getItems()
+
+//    $intToBlogPostArray->pushItem(new \stdClass());                                 //Adding objects of incorrect class throw a TypeError
 }
 
-$intToBlogPostArray = new IntToBlogPostArray();                                     //Make new Array object
-
-                                                                                    //Can add objects of correct class successfully
-$intToBlogPostArray->pushItem(new BlogPost('This is a blog post about cats'));      //- via pushItem()
-$intToBlogPostArray->setItem(1, new BlogPost('This is a blog post about food'));    //- via setItem() to specify the key too
 
 show_blog_posts_content($intToBlogPostArray);                                       //Pass array to function with full type hinting
-
-//$intToBlogPostArray->pushItem(new stdClass());                                    //Exception because of wrong class
