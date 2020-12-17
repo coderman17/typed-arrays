8ecf2afaf392e7560608c21247d474eeda7eeab1
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-12-17 16:32:18 +0000

    IntToString behaviour not implementation

--- tests/IntToValueArrays/IntToStringArrayTest.php
+++ tests/IntToValueArrays/IntToStringArrayTest.php
@@ -60,6 +60,32 @@ final class IntToStringArrayTest extends TestCase
         );
     }
 
+    //unsetItem:
+
+    public function testUnsetItem(): void
+    {
+        $this->array->setItem(0, 'a');
+
+        $this->array->setItem(1, 'b');
+
+        $this->array->unsetItem(0);
+
+        $this::assertSame(
+            [
+                1 => 'b'
+            ],
+            $this->array->getItems()
+        );
+    }
+
+    public function testUnsetItemKeyIsTypeInt(): void
+    {
+        $this::assertSame(
+            'int',
+            TestHelpers::getParameterType($this->fullyQualifiedClassName, 'unsetItem', 'key', $this)
+        );
+    }
+
     //pushItem:
 
     public function testPushItem(): void
@@ -108,5 +134,98 @@ final class IntToStringArrayTest extends TestCase
         $this::expectException('TypeError');
 
         $this->array[0] = 0;
+    }
+
+    //offsetGet:
+
+    public function testOffsetGet(): void
+    {
+        $this->array->setItem(0, 'a');
+
+        $this::assertSame(
+            'a',
+            $this->array[0]
+        );
+    }
+
+    public function testOffsetGetKeyError(): void
+    {
+        $this::expectException('TypeError');
+
+        echo $this->array['0'];
+    }
+
+    //offsetUnset:
+
+    public function testOffsetUnset(): void
+    {
+        $this->array->setItem(0, 'a');
+
+        $this->array->setItem(1, 'b');
+
+        unset($this->array[0]);
+
+        $this::assertSame(
+            [
+                1 => 'b'
+            ],
+            $this->array->getItems()
+        );
+    }
+
+    public function testOffsetUnsetKeyError(): void
+    {
+        $this::expectException('TypeError');
+
+        unset($this->array['0']);
+    }
+
+    //offsetExists:
+
+    public function testOffsetExists(): void
+    {
+        $this->array->setItem(0, 'a');
+
+        $this::assertSame(
+            true,
+            isset($this->array[0])
+        );
+    }
+
+    public function testOffsetExistsKeyError(): void
+    {
+        $this::expectException('TypeError');
+
+        echo isset($this->array['0']);
+    }
+
+    //countable:
+
+    public function testCountable(): void
+    {
+        $this->array->setItem(0, 'a');
+
+        $this::assertSame(
+            1,
+            count($this->array)
+        );
+    }
+
+    //Iterator:
+
+    public function testIterator(): void
+    {
+        $this->array->setItem(0, 'a');
+
+        foreach ($this->array as $key => $value) {
+            $this::assertSame(
+                0,
+                $key
+            );
+            $this::assertSame(
+                'a',
+                $value
+            );
+        }
     }
 }
