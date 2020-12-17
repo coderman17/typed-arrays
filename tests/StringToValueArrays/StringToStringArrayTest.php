85e9657fe8378d6f6a6c79fa4bd4f9f618bd41c8
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-12-17 18:17:36 +0000

    use string to string array property in tests not local variable

--- tests/StringToValueArrays/StringToStringArrayTest.php
+++ tests/StringToValueArrays/StringToStringArrayTest.php
@@ -12,31 +12,33 @@ final class StringToStringArrayTest extends TestCase
 {
     protected string $fullyQualifiedClassName;
 
+    protected StringToStringArray $array;
+
     public function setUp(): void
     {
         parent::setUp();
 
         $this->fullyQualifiedClassName = 'TypedArrays\StringToValueArrays\StringToStringArray';
+
+        $this->array = new StringToStringArray();
     }
 
     //setItem:
 
     public function testSetItem(): void
     {
-        $stringToStringArray = new StringToStringArray();
-
-        $setMethod = function ($key, $value) use ($stringToStringArray){
-            $stringToStringArray->setItem($key, $value);
+        $setMethod = function ($key, $value){
+            $this->array->setItem($key, $value);
         };
 
         TestHelpers::checkForSilentKeyTypeCastingException($setMethod, 'a', $this);
 
         //These tests check that PHP isn't quietly converting string keys to integers
         foreach (TestHelpers::STRING_KEYS_PHP_WILL_NOT_CAST_AS_INT as $stringKey){
-            $stringToStringArray->setItem($stringKey, 'a');
+            $this->array->setItem($stringKey, 'a');
         }
 
-        foreach ($stringToStringArray->getItems() as $k => $v){
+        foreach ($this->array->getItems() as $k => $v){
             $this::assertIsString($k);
             $this::assertIsString($v);
         }
@@ -62,20 +64,18 @@ final class StringToStringArrayTest extends TestCase
 
     public function testOffsetSet(): void
     {
-        $stringToStringArray = new StringToStringArray();
-
-        $offsetSetMethod = function ($key, $value) use ($stringToStringArray){
-            $stringToStringArray[$key] = $value;
+        $offsetSetMethod = function ($key, $value){
+            $this->array[$key] = $value;
         };
 
         TestHelpers::checkForSilentKeyTypeCastingException($offsetSetMethod, 'a', $this);
 
         //These tests check that PHP isn't quietly converting string keys to integers
         foreach (TestHelpers::STRING_KEYS_PHP_WILL_NOT_CAST_AS_INT as $stringKey){
-            $stringToStringArray[$stringKey] = 'a';
+            $this->array[$stringKey] = 'a';
         }
 
-        foreach ($stringToStringArray->getItems() as $k => $v){
+        foreach ($this->array->getItems() as $k => $v){
             $this::assertIsString($k);
             $this::assertIsString($v);
         }
@@ -83,15 +83,15 @@ final class StringToStringArrayTest extends TestCase
 
     public function testOffsetSetKeyError(): void
     {
-        $stringToStringArray = new StringToStringArray();
         $this::expectException('TypeError');
-        $stringToStringArray[0] = '0';
+
+        $this->array[0] = '0';
     }
 
     public function testOffsetSetValueError(): void
     {
-        $stringToStringArray = new StringToStringArray();
         $this::expectException('TypeError');
-        $stringToStringArray['0'] = 0;
+
+        $this->array['0'] = 0;
     }
 }
